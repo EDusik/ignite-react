6e37135de452d9892624aae9a12a6311f5a0f342
EDusik <eduardodusik@gmail.com>
2021-03-21 16:38:49 -0300

    02/04-07 - styling modal buttons with transparentize property

--- 02-dt-money/dtmoney/src/components/NewTransactionModal/index.tsx
+++ 02-dt-money/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -38,6 +38,7 @@ export const NewTransactionModal = ({ isOpen, onRequestClose } : NewTransactionM
             type="button"
             onClick={() => { setType("deposit") }}
             isActive={type === "deposit"}
+            activeColor="green"
           > 
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
@@ -47,6 +48,7 @@ export const NewTransactionModal = ({ isOpen, onRequestClose } : NewTransactionM
             type="button"  
             onClick={() => { setType("withdraw") }} 
             isActive={type === "withdraw"}
+            activeColor="red"
           >
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
