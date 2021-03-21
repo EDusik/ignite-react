b286a5e99a69c7e49c4148707c9c876c6debb9ad
EDusik <eduardodusik@gmail.com>
2021-03-21 18:44:40 -0300

    02/04-11 - formating number and data with 'Intl'

--- 02-dt-money/dtmoney/src/index.tsx
+++ 02-dt-money/dtmoney/src/index.tsx
@@ -40,7 +40,6 @@ createServer({
 
     this.post("/transactions", (schema, request) => {
       const data = JSON.parse(request.requestBody);
-
       return schema.create("transaction", data);
     });
   }
