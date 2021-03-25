81cabd652873b47e803872a7b817ce808066e55b
EDusik <eduardodusik@gmail.com>
2021-03-25 20:54:55 -0300

    03/02-01 - add header component

--- module-03/ignews/pages/_app.tsx
+++ module-03/ignews/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { AppProps } from "next/app";
+import React from "react";
+import { Header } from "../src/components/Header";
 
 import "../src/styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
