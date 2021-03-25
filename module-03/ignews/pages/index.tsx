cfc39375baefd65134fc576b4d617e79440e266c
EDusik <eduardodusik@gmail.com>
2021-03-24 22:28:15 -0300

    03/01-07 - dynamic title

--- module-03/ignews/pages/index.tsx
+++ module-03/ignews/pages/index.tsx
@@ -1,5 +1,13 @@
 import styles from "../src/styles/home.module.scss";
+import Head from "next/head";
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello World</h1>;
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>Hello World</h1>
+    </>
+  )
 }
