f4e0af5eb565be6b78cb360141f3ecfb5baa4993
EDusik <eduardodusik@gmail.com>
2021-03-27 16:54:39 -0300

    03/03-03 - changing getServerSideProps to getStaticProps

--- module-03/ignews/pages/index.tsx
+++ module-03/ignews/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../src/components/SubscribeButton";
 
@@ -35,7 +35,12 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+// Call API
+// Client-side
+// Server-side 
+// Static Site Generations
+
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve("price_1IZg46HW2kzcyQbCHEdonefz", {
     expand: ["product"]
@@ -52,6 +57,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 25 hours
   }
 }
