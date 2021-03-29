59b81b280c9dd171a0855acf6ec57b2abdd5ab0e
EDusik <eduardodusik@gmail.com>
2021-03-29 20:26:50 -0300

    03-02/01-04 - adding GitHub NextAuth

--- module-03/ignews/pages/_app.tsx
+++ module-03/ignews/pages/_app.tsx
@@ -1,15 +1,16 @@
 import { AppProps } from "next/app";
 import React from "react";
 import { Header } from "../src/components/Header";
+import { Provider as NextAuthProvider } from "next-auth/client";
 
 import "../src/styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   );
 }
 
