5fd67c9ec7d7dfa043a208c5b18f3b6b40dafdbb
EDusik <eduardodusik@gmail.com>
2021-04-01 21:45:49 -0300

    03-02/04-04 - saving data event - user and subscription at faunadb

--- module-03/ignews/pages/api/webhooks.ts
+++ module-03/ignews/pages/api/webhooks.ts
@@ -1,7 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
+
 import { stripe } from "../../src/services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -21,7 +23,9 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-  "checkout.session.completed"
+  "checkout.session.completed",
+  "customer.subscription.updated",
+  "customer.subscription.deleted"
 ]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -39,9 +43,38 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      console.log("Evento Recebido", event);
+      try {
+        switch(type) {
+          case "customer.subscription.updated":
+          case "customer.subscription.deleted":
+
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+
+            break;
+          case "checkout.session.completed":
+            const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+              true
+            );
+
+            break;
+          default:
+            throw new Error("Unhandled event.");
+        }
+      } catch(err) {
+        return res.json({error: "Webhook handler failed."});
+      }
     }
-    
+
     res.json({ received: true });
   } else {
     res.setHeader("Allow", "Posts");
