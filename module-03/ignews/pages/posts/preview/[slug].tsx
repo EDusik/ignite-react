b6945bd4a99997b14e0d4fa86c8e53aef02d7459
EDusik <eduardodusik@gmail.com>
2021-04-06 14:17:10 -0300

    03-03/02-04 - Final JAMStack

--- module-03/ignews/pages/posts/preview/[slug].tsx
+++ module-03/ignews/pages/posts/preview/[slug].tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import Head from "next/head";
 import Link from "next/link";
 
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/client";
 
@@ -57,7 +57,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
   )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
     fallback: "blocking"
@@ -84,6 +84,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
     props: {
       post
-    }
+    },
+    redirect: 60 * 30, // 30 minutes
   };
 }
