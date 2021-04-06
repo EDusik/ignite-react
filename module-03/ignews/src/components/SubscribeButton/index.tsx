aabcba053b93bc59d69ec983e6a3ccce9b758c00
EDusik <eduardodusik@gmail.com>
2021-04-06 13:47:57 -0300

    03-03/02-03 - adding page preview

--- module-03/ignews/src/components/SubscribeButton/index.tsx
+++ module-03/ignews/src/components/SubscribeButton/index.tsx
@@ -19,7 +19,7 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
       return;
     }
 
-    if (section.activeSubscription) {
+    if (section?.activeSubscription) {
       router.push("/posts")
       return;
     }
