1ace516abd547e81358627eef91d6ee07feb1405
EDusik <eduardodusik@gmail.com>
2021-04-14 22:10:52 -0300

    04/02-01 - adding login page using chakra-ui

--- module-04/dashgo/src/pages/index.tsx
+++ module-04/dashgo/src/pages/index.tsx
@@ -1,5 +1,61 @@
+import {
+  Flex, Input, Button, Stack, FormLabel, FormControl
+} from "@chakra-ui/react";
+
 export default function Home() {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justifyContent="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        padding="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-Mail</FormLabel>
+            <Input
+              type="email"
+              name="email"
+              id="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900"
+              }}
+              size="lg"
+              placeholder="E-mail"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              type="password"
+              name="password"
+              id="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900"
+              }}
+              size="lg"
+              placeholder="Senha"
+            />
+          </FormControl>
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
