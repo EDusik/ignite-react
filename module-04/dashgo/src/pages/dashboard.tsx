f5dbed02722852dc57e3e3c9a2dcd996cd8a50a6
EDusik <eduardodusik@gmail.com>
2021-04-23 13:56:41 -0300

    04/04-05 - responsive Dashboard

--- module-04/dashgo/src/pages/dashboard.tsx
+++ module-04/dashgo/src/pages/dashboard.tsx
@@ -68,16 +68,16 @@ export default function Dashboard() {
   return (
     <Flex direction="column" h="100vh">
       <Header />
-      <Flex maxWidth="{1480}" w="100%" my="6"mx="auto" px="6">
+      <Flex maxWidth={1480} w="100%" my="6" mx="auto" px="6">
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">Inscritos da semana</Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
 
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8}>
             <Text fontSize="lg" mb="4">Taxa de abertura</Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
