cfad12a53d702b20fbe3c2faf990e1993ac4fd52
EDusik <eduardodusik@gmail.com>
2021-04-23 13:48:04 -0300

    04/04-04 - responsive Sidebar  with context

--- module-04/dashgo/src/components/Header/index.tsx
+++ module-04/dashgo/src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue, } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
 
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./NotificationsNav";
@@ -6,6 +8,7 @@ import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
@@ -22,6 +25,15 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          aria-label="Open navigation"
+          mr="2">
+        </IconButton>
+      )}
       <Logo />
       {isWideVersion &&  <SearchBox />}
       
