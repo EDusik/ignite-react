732511308162cec2f53f771a8a56c7b1d12ff8c3
EDusik <eduardodusik@gmail.com>
2021-05-03 11:29:57 -0300

    04/02-01-05 - add listing users

--- module-04/dashgo/src/pages/users/index.tsx
+++ module-04/dashgo/src/pages/users/index.tsx
@@ -35,7 +35,20 @@ export default function UserList() {
     const response = await fetch("http://localhost:3000/api/users")
     const data = await response.json();
 
-    return data;
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+          day: "2-digit",
+          month: "long",
+          year: "numeric"
+        })
+      }
+    });
+
+    return users;
   });
 
   return (
@@ -79,35 +92,39 @@ export default function UserList() {
                       <Checkbox colorScheme="pink" />
                     </Th>
                     <Th>Usuário</Th>
-                    {isWideVersion && <Th>Data de cadastro</Th>}
+                      {isWideVersion && <Th>Data de cadastro</Th>}
                     <Th width="8"></Th>
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={["4", "4" ,"6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Eduardo Dusik</Text>
-                        <Text fontSize="sm" color="gray.300">eduardodusik@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                    <Td> 
-                      {isWideVersion && (
-                        <Button 
-                          as="a" 
-                          size="sm" 
-                          fontSize="sm" 
-                          colorScheme="purple" 
-                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
-                            Editar
-                        </Button>
-                      )}
-                    </Td>
-                  </Tr>
+                  {data.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4" ,"6"]}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">{user.name}</Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                        <Td> 
+                          {isWideVersion && (
+                            <Button 
+                              as="a" 
+                              size="sm" 
+                              fontSize="sm" 
+                              colorScheme="purple" 
+                              leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                                Editar
+                            </Button>
+                          )}
+                        </Td>
+                      </Tr>
+                    )
+                  })}                
                 </Tbody>
               </Table>
               <Pagination />
