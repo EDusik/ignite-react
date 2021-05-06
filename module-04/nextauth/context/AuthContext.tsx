9aeaa923940a3bc462ebb6b9912256bc2a986276
EDusik <eduardodusik@gmail.com>
2021-05-06 18:23:40 -0300

    04/03-02-02 - adding axios config

--- module-04/nextauth/context/AuthContext.tsx
+++ module-04/nextauth/context/AuthContext.tsx
@@ -1,5 +1,7 @@
 import { createContext, ReactNode } from "react";
 
+import { api } from "../services/api";
+
 type SignInCredentials = {
   email: string;
   password: string;
@@ -20,7 +22,16 @@ export function AuthProvider ({  children }: AuthProviderProps ) {
   const isAuthenticated = false;
 
   async function signIn({ email, password }: SignInCredentials) {
-    console.log({ email, password });
+    try { 
+      const response = await api.post("sessions", {
+        email,
+        password
+      });
+  
+      console.log(response.data)
+    } catch (err) {
+      console.log(err);
+    }  
   }
 
   return (
