83ab64483a1d3b9f0bc2a41e847683bf9d6cfca0
EDusik <eduardodusik@gmail.com>
2021-05-10 18:02:10 -0300

    04/03-04-04 - adding BroadcastChannel signOut()

--- module-04/nextauth/pages/dashboard.tsx
+++ module-04/nextauth/pages/dashboard.tsx
@@ -8,7 +8,7 @@ import { setupAPIClient } from "../services/api";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   useEffect(() => {
     api.get("/me")
@@ -19,6 +19,7 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard {user?.email}</h1>
+      <button onClick={signOut}>Sign out</button>
       <Can permissions={["metrics.list"]}>
         <div>Dashboard</div>
       </Can>
