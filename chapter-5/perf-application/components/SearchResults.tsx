d69da34313ad1d0fe1f3277f52dff1b625ce45fe
EDusik <eduardodusik@gmail.com>
2021-05-18 20:10:38 -0300

    05/02-06 - adding react-virtualized lib

--- chapter-5/perf-application/components/SearchResults.tsx
+++ chapter-5/perf-application/components/SearchResults.tsx
@@ -1,3 +1,5 @@
+import { List, ListRowRenderer } from "react-virtualized";
+
 import React from 'react';
 import { ProductItem } from "./ProductItem";
 
@@ -14,18 +16,30 @@ interface SearchResultsProps {
 
 export function SearchResults({ results, totalPrice, onAddToWishList }: SearchResultsProps) {
 
+
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem 
+          product={results[index]} 
+          onAddToWishList={onAddToWishList}
+        />
+      </div> 
+    )
+  }
+
   return (
     <div>
       <h2>{totalPrice}</h2>
-      {results.map(product => {
-        return (
-          <ProductItem 
-            key={product.id}
-            product={product} 
-            onAddToWishList={onAddToWishList}
-          />
-        )
-      })}
+
+      <List 
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   )
 }
